7c5bebe136d97f6c0351d851e401ea0bcd7ea50e
Yash Nahata <nahata.y@northeastern.edu>
2024-02-20 12:27:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "google_compute_subnetwork" "db" {
   network       = google_compute_network.my_vpc.id
   ip_cidr_range = var.db_subnet_cidr
   region        = var.region
-
+}
 
 resource "google_compute_route" "default_route" {
   name             = var.default_route_name
